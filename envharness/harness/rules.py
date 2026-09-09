eb2a9996c8f11e4b4981470b3c1400fa196316b9
KindofCrazy <221900275@smail.nju.edu.cn>
2026-09-09 10:05:15 +0800

    M3.2-r1

--- envharness/harness/rules.py
+++ envharness/harness/rules.py
@@ -8,20 +8,25 @@ class Rules(EnvHarness):
     def filter_action(self, action: Action) -> Action:
         return action
 
-    def filter_transition(self, action: Action, response: EnvResponse) -> EnvResponse:
+    def modify_transition(self, action: Action, response: EnvResponse) -> EnvResponse:
         return response
 
     def filter_observation(self, observation: Observation) -> Observation:
         return observation
 
-    def step(self, action):
+    def step(self, action: Action) -> EnvResponse:
         action = self.filter_action(action)
-        response = super().step()
-        response = self.filter_transition(action, response)
+        response = super().step(action)
+        response = self.modify_transition(action, response)
         response.observation = self.filter_observation(response.observation)
 
         return response
 
-    def observe(self):
+    def observe(self) -> Observation:
         observation = super().observe()
         return self.filter_observation(observation)
+
+    def reset(self, *args, **kwargs) -> Observation:
+        observation = super().reset(*args, **kwargs)
+        return self.filter_observation(observation)
+    
